4ac552e019df4e93fb15428894e26e7fe4d6bb43
Karso2023 <karsoo2023@gmail.com>
2026-05-22 02:19:11 +0100

    done i2c.rs, this is for finding the related bus

--- sound-fix/src/i2c.rs
+++ sound-fix/src/i2c.rs
@@ -1,7 +1,10 @@
 use std::fs;
 use std::path::Path;
 
-pub fn find_tias2781_device() -> Option<String> {
+use i2cdev::linux::LinuxI2CDevice;
+
+
+pub fn find_tias2781_device() -> Option<u8> {
     let path = Path::new("/sys/bus/i2c/devices/");
 
     if let Ok(entries) = fs::read_dir(path) {
@@ -10,10 +13,34 @@ pub fn find_tias2781_device() -> Option<String> {
 
             if let Some(name) = path.file_name() {
                 if name.to_string_lossy().contains("TIAS2781") {
-                    return Some(path.to_string_lossy().into_owned());
+
+                    // read the symlink target + find the right bus
+                    if let Ok(target) = fs::read_link(&path) {
+                        for component in target.components() {
+                            if let Some(s) = component.as_os_str().to_str() {
+                                if s.starts_with("i2c-") && s[4..].chars().all(|x| x.is_ascii_digit()) {
+                                    if let Some(bus) = s.strip_prefix("i2c-").and_then(|n| n.parse::<u8>().ok()) {
+                                        return Some(bus);
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
     }
-    None 
+
+    // open bus directly if cant find sysfs entry 
+    for bus_num in 0u8..= 9 {
+        let dev_path = format!("/dev/i2c-{}", bus_num);
+        if LinuxI2CDevice::new(&dev_path, 0x38).is_ok() {
+            return Some(bus_num);
+        }
+        if LinuxI2CDevice::new(&dev_path, 0x3d).is_ok() {
+            return Some(bus_num);
+        }
+    }
+    
+    None
 }
